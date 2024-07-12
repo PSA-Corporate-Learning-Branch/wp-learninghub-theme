4d41062277ceef296341aeccaaf7345486c5afd6
Nori Sinclair <Nori.Sinclair@gov.bc.ca>
2024-07-12 19:19:28 +0000

    style edits for dark mode css

--- front-page.php
+++ front-page.php
@@ -7,7 +7,7 @@
     </div>
 </div>
 
-<div class="container-lg p-4 p-md-5">
+<div class="container-lg pt-4 pt-md-5 px-4 px-md-5">
     <div class="row">
         <div class="col-lg-8 mb-4 mb-lg-0">
             <h2 class="fs-1">What is corporate learning?</h2>
@@ -15,8 +15,8 @@
             <a href="/learninghub/filter/" class="btn btn-lg btn-primary">Catalogue of courses</a>
             <div class="topic-card border-2 border rounded shadow-sm p-3 mt-4">
                 <div class="d-flex">
-                    <div class="icon-square flex-shrink-0 mt-1 "><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-                            <path fill="#007864" d="M64 32C64 14.3 49.7 0 32 0S0 14.3 0 32V64 368 480c0 17.7 14.3 32 32 32s32-14.3 32-32V352l64.3-16.1c41.1-10.3 84.6-5.5 122.5 13.4c44.2 22.1 95.5 24.8 141.7 7.4l34.7-13c12.5-4.7 20.8-16.6 20.8-30V66.1c0-23-24.2-38-44.8-27.7l-9.6 4.8c-46.3 23.2-100.8 23.2-147.1 0c-35.1-17.6-75.4-22-113.5-12.5L64 48V32z" />
+                    <div class="icon-square flex-shrink-0 mt-1"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="green-flag"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                            <path d="M64 32C64 14.3 49.7 0 32 0S0 14.3 0 32V64 368 480c0 17.7 14.3 32 32 32s32-14.3 32-32V352l64.3-16.1c41.1-10.3 84.6-5.5 122.5 13.4c44.2 22.1 95.5 24.8 141.7 7.4l34.7-13c12.5-4.7 20.8-16.6 20.8-30V66.1c0-23-24.2-38-44.8-27.7l-9.6 4.8c-46.3 23.2-100.8 23.2-147.1 0c-35.1-17.6-75.4-22-113.5-12.5L64 48V32z" />
                         </svg></div>
                     <div class="ms-3">
                         <h3 class="gov-green">Start here</h3>
@@ -34,7 +34,7 @@
             <div class="mt-4 card shadow-sm rounded">
                 <img src="http://nori.virtuallearn.ca/learninghub/wp-content/uploads/2024/03/Home2.jpg" class="card-img-top object-fit-cover rounded-top" alt="" style="height:18vh; ">
                 <div class="card-body">
-                    <h3 class="text-black mb-3">What's new?</h3>
+                    <h3 class="mb-3 text-reset">What's new?</h3>
                     <h4>Improved search</h4>
                     <p class="card-text">Explore new features including course search filters and learning journeys.</p>
                     <a href="/learninghub/about/">About the LearningHUB</a>
@@ -72,7 +72,7 @@
 </div>
 
 
-<div class="container">
+<div class="container-lg pt-4 pt-md-5 px-4 px-md-5 bg-light">
     <div class="row">
         <div style="padding: 1em;">
             <h3>How is learning organized?</h3>
@@ -84,9 +84,9 @@
             </p>
             <div class="my-3 p-1">
                 <div class="row mb-lg-3 mb-0">
-                    <div class="col-lg-6 mb-3 mb-lg-0 h-100">
+                    <div class="col-lg-6 mb-3 mb-lg-0 h-100" >
                         <div class="shadow-sm">
-                            <div class="d-flex bg-gov-green px-3 py-2 rounded-top mb-2">
+                            <div class="d-flex bg-gov-green px-3 py-2 rounded-top mb-2" >
                                 <div class="icon-square flex-shrink-0">
                                     <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 576 512" class="icon "><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                                         <path fill="#ffffff" d="M264.5 5.2c14.9-6.9 32.1-6.9 47 0l218.6 101c8.5 3.9 13.9 12.4 13.9 21.8s-5.4 17.9-13.9 21.8l-218.6 101c-14.9 6.9-32.1 6.9-47 0L45.9 149.8C37.4 145.8 32 137.3 32 128s5.4-17.9 13.9-21.8L264.5 5.2zM476.9 209.6l53.2 24.6c8.5 3.9 13.9 12.4 13.9 21.8s-5.4 17.9-13.9 21.8l-218.6 101c-14.9 6.9-32.1 6.9-47 0L45.9 277.8C37.4 273.8 32 265.3 32 256s5.4-17.9 13.9-21.8l53.2-24.6 152 70.2c23.4 10.8 50.4 10.8 73.8 0l152-70.2zm-152 198.2l152-70.2 53.2 24.6c8.5 3.9 13.9 12.4 13.9 21.8s-5.4 17.9-13.9 21.8l-218.6 101c-14.9 6.9-32.1 6.9-47 0L45.9 405.8C37.4 401.8 32 393.3 32 384s5.4-17.9 13.9-21.8l53.2-24.6 152 70.2c23.4 10.8 50.4 10.8 73.8 0z" />
@@ -117,7 +117,7 @@
                     </div>
                     <div class="col-lg-6 mb-3 mb-lg-0 h-100">
                         <div class="shadow-sm">
-                            <div class="d-flex bg-gov-green px-3 py-2 rounded-top mb-2">
+                            <div class="d-flex bg-gov-green px-3 py-2 rounded-top mb-2" >
                                 <div class="icon-square flex-grow-0 flex-shrink-0">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" class="icon"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                                         <path fill="#fff" d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192h42.7c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0H21.3C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7h42.7C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3H405.3zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352H378.7C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7H154.7c-14.7 0-26.7-11.9-26.7-26.7z" />
@@ -150,7 +150,7 @@
                     </div>
                 </div>
                 <div class="mt-lg-3 mb-3">
-                    <div class="d-flex bg-gov-green px-3 py-2 rounded-top mb-2">
+                    <div class="d-flex bg-gov-green px-3 py-2 rounded-top mb-2" >
                         <div class="icon-square flex-shrink-0">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="icon"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                                 <path fill="#ffffff" d="M0 80V229.5c0 17 6.7 33.3 18.7 45.3l176 176c25 25 65.5 25 90.5 0L418.7 317.3c25-25 25-65.5 0-90.5l-176-176c-12-12-28.3-18.7-45.3-18.7H48C21.5 32 0 53.5 0 80zm112 32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" />
