ff1ea1b4904fa2c1572475240228ee4f56d8bae9
Allan Haggett <Allan.Haggett@gov.bc.ca>
2024-07-16 22:21:44 +0000

    Initial UI at least showing real numbers.

--- page-taxonomy-audit.php
+++ page-taxonomy-audit.php
@@ -14,34 +14,36 @@ get_header();
 $post_args = array(
     'post_type'                => 'course',
     'post_status'              => 'publish',
-    'posts_per_page'           => -1,
-    'ignore_sticky_posts'      => 0,
-    'tax_query' 			   => array(
-									'taxonomy' => 'topics',
-									'terms' => '',
-									'operator' => 'NOT EXISTS'
-								),
-    'orderby'                  => array(
-									'date' =>'DESC',
-									'menu_order'=>'ASC'
-								), 
-    'order'                    => 'ASC',
-    'hide_empty'               => 1,
-    'hierarchical'             => 1,
-    'exclude'                  => '',
-    'include'                  => '',
-    'number'                   => '',
-    'pad_counts'               => false, 
-	//'s'						   => 'ethics'
+    'posts_per_page'           => -1
 );
 $post_my_query = null;
 $post_my_query = new WP_Query($post_args);
-
+$nogroups = [];
+$noauds = [];
+$notops = [];
+$nomethods = [];
+if( $post_my_query->have_posts() ) : 
+while ($post_my_query->have_posts()) : $post_my_query->the_post(); 
+    $name = get_the_title();
+    $slug = basename(get_permalink());
+    $grp = get_the_terms($post_my_query->ID, 'groups', '', ', ', ' ');
+    $top = get_the_terms($post_my_query->ID, 'topics', '', ', ', ' ');
+    $aud = get_the_terms($post_my_query->ID, 'audience', '', ', ', ' ');
+    $dm = get_the_terms($post_my_query->ID, 'delivery_method', '', ', ', ' ');
+    $ext = get_the_terms($post_my_query->ID, 'external_system', '', ', ', ' ');
+    $taxs = [$grp[0]->name,$top[0]->name,$aud[0]->name,$dm[0]->name,$ext[0]->name];
+    $c = [$slug,$name,$taxs];
+     if(empty($grp[0]->name)) array_push($nogroups, $c);
+     if(empty($top[0]->name)) array_push($notops, $c);
+     if(empty($aud[0]->name)) array_push($noauds, $c);
+     if(empty($dm[0]->name)) array_push($nomethods, $c);
+endwhile;
+endif; 
 ?>
 <div class="bg-gov-green">
 <div class="container">
-<div class="row py-5 mb-3">
-<div class="col-md-12">
+<div class="row py-5 mb-3 justify-content-md-center">
+<div class="col-md-8">
 	<h1>Taxonomy Audit</h1>
 </div>
 </div>
@@ -50,24 +52,57 @@ $post_my_query = new WP_Query($post_args);
 <div class="container">
 <div class="row justify-content-md-center">
 <div class="col-md-8">
-<?php $count = 0 ?>
-<?php if( $post_my_query->have_posts() ) : ?>
-<?php while ($post_my_query->have_posts()) : $post_my_query->the_post(); ?>
 
-<div>
-	<?php $count ++; echo $count; ?>
-	<a href="<?= the_permalink() ?>"><?= the_title() ?></a>
+<p>Choose a taxonomy to see courses in the catalogue which do NOT have a term applied.</p>
+
+<details>
+<summary>Topic <span class="badge bg-warning text-black"><?= count($notops) ?></span></summary>
+<?php foreach($notops as $nt): ?>
+<div class="p-3 mb-3 bg-light-subtle rounded-3">
+<div><a href="/learninghub/course/<?= $nt[0] ?>"><?= $nt[1] ?></a></div>
+<div>Platform: <?= $nt[2][4] ?></div>
+<div>Group: <?= $nt[2][0] ?></div>
+<div>Topic: <?= $nt[2][1] ?></div>
+<div>Audience: <?= $nt[2][2] ?></div>
+<div>Delivery: <?= $nt[2][3] ?></div>
 </div>
+<?php endforeach ?>
+</details>
 
-<?php endwhile; ?>
-<?php else : ?>
-
-<p>Sorry, but there are no courses that match your filters.</p>
-
-<?php endif; ?>
-
-
+<details>
+<summary>Group <span class="badge bg-warning text-black"><?= count($nogroups) ?></span></summary>
+<?php foreach($nogroups as $ng): ?>
+<div class="p-3 mb-3 bg-light-subtle rounded-3">
+<div><a href="/learninghub/course/<?= $ng[0] ?>"><?= $ng[1] ?></a></div>
+<div>Platform: <?= $ng[2][4] ?></div>
+<div>Group: <?= $ng[2][0] ?></div>
+<div>Topic: <?= $ng[2][1] ?></div>
+<div>Audience: <?= $ng[2][2] ?></div>
+<div>Delivery: <?= $ng[2][3] ?></div>
 </div>
+<?php endforeach ?>
+</details>
+
+<details>
+<summary>Audience <span class="badge bg-warning text-black"><?= count($noauds) ?></span></summary>
+<?php foreach($noauds as $a): ?>
+<div class="p-3 mb-3 bg-light-subtle rounded-3">
+<div><a href="/learninghub/course/<?= $a[0] ?>"><?= $a[1] ?></a></div>
+<div>Platform: <?= $a[2][4] ?></div>
+<div>Group: <?= $a[2][0] ?></div>
+<div>Topic: <?= $a[2][1] ?></div>
+<div>Audience: <?= $a[2][2] ?></div>
+<div>Delivery: <?= $a[2][3] ?></div>
+</div>
+<?php endforeach ?>
+</details>
+
+<details>
+<summary>Delivery Method <span class="badge bg-warning text-black"><?= count($nomethods) ?></span></summary>
+<?php foreach($nomethods as $m): ?>
+<div><a href="/learninghub/course/<?= $m[0] ?>"><?= $m[1] ?></a></div>
+<?php endforeach ?>
+</details>
 </div>
 </div>
 <?php 	
