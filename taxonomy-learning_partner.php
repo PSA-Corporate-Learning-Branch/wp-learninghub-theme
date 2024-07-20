64d8b1dad24b724e4401dc328b0ba48fc43fe305
Nori Sinclair <Nori.Sinclair@gov.bc.ca>
2024-07-20 09:00:25 +0000

    Partner course list page layout

--- taxonomy-learning_partner.php
+++ taxonomy-learning_partner.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying archive pages
  *
@@ -14,11 +15,11 @@ get_header();
 $termtitle = get_the_archive_title();
 $description = get_the_archive_description();
 $taxquery = array(
-	array (
-		'taxonomy' => 'learning_partner',
-		'field' => 'slug',
-		'terms' => sanitize_text_field(get_query_var('learning_partner')),
-	)
+    array(
+        'taxonomy' => 'learning_partner',
+        'field' => 'slug',
+        'terms' => sanitize_text_field(get_query_var('learning_partner')),
+    )
 );
 
 $post_args = array(
@@ -26,96 +27,92 @@ $post_args = array(
     'post_status'              => 'publish',
     'posts_per_page'           => -1,
     'ignore_sticky_posts'      => 0,
-    'tax_query' 			   => $taxquery,
-    'orderby'                  => 'name', 
+    'tax_query'                => $taxquery,
+    'orderby'                  => 'name',
     'order'                    => 'ASC',
     'hide_empty'               => 0,
     'hierarchical'             => 1,
     'exclude'                  => '',
     'include'                  => '',
     'number'                   => '',
-    'pad_counts'               => true, 
+    'pad_counts'               => true,
 );
 $post_my_query = null;
 $post_my_query = new WP_Query($post_args);
 
 ?>
 
-<div class="bg-gov-green">
-<div class="container">
-<div class="row py-5 mb-3">
-<div class="col-12">
+<div id="content">
+    <div class="d-flex p-4 p-md-5 align-items-center bg-gov-green bg-gradient" style="height: 12vh; min-height: 100px;">
+        <div class="container-lg py-4 py-md-5">
+            <h1 class="text-white title">Partner: <?= str_replace('Learning Partners:', '', $termtitle) ?></h1>
+        </div>
+    </div>
+    <div class="bg-secondary-subtle">
+        <div class="container-lg p-lg-5 p-4 bg-light-subtle">
+            <h2>Courses offered by the <?= str_replace('Learning Partners:', '', $termtitle) ?></h2>
+            <?php
+            $partners = get_categories(
+                array(
+                    'taxonomy' => 'learning_partner',
+                    'orderby' => 'id',
+                    'order' => 'ASC',
+                    'hide_empty' => '1'
+                )
+            );
+            ?>
 
-    <h1 class=""><?= str_replace('Learning Partners:','',$termtitle) ?></h1>
-	<?php 
-	$partners = get_categories(
-							array(
-								'taxonomy' => 'learning_partner',
-								'orderby' => 'id',
-								'order' => 'DESC',
-								'hide_empty' => '1'
-							));
-	?>
-	
-	<div class="dropdown">
-	<button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-		Partners
-	</button>
-	<ul class="dropdown-menu">
-		<?php foreach($partners as $p): ?>
-		<li>
-			<a class="dropdown-item" href="/learninghub/learning_partner/<?= $p->slug ?>">
-				<?= $p->name ?> (<?= $p->count ?>)
-			</a>
-		</li>
-		<?php endforeach ?>
-	</ul>
-	</div>
+            <div class="dropdown mb-3">
+                <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Select another partner
+                </button>
+                <ul class="dropdown-menu">
+                    <?php foreach ($partners as $p) : ?>
+                        <li>
+                            <a class="dropdown-item" href="/learninghub/learning_partner/<?= $p->slug ?>">
+                                <?= $p->name ?> (<?= $p->count ?>)
+                            </a>
+                        </li>
+                    <?php endforeach ?>
+                </ul>
+            </div>
 
-</div>
-</div>
-</div>
-</div>
+            <div class="row">
+                <div class="col-md-8">
 
-<div class="container">
-	<div class="row">
-	<div class="col-md-8">
-	
-	
-	<?php if( $post_my_query->have_posts() ) : ?>
-		<div class="mb-2"><strong><?= $post_my_query->found_posts ?> courses on this platform.</strong></div>
-	<?php while ($post_my_query->have_posts()) : $post_my_query->the_post(); ?>
-		<?php get_template_part( 'template-parts/course/single-course' ) ?>
-	<?php endwhile; ?>
-	<?php else : ?>
-		<p>Oh no! There are no courses that match your filters.</p>
-	<?php //get_template_part( 'template-parts/content/content-none' ); ?>
-	<?php endif; ?>
+                    <?php if ($post_my_query->have_posts()) : ?>
+                        <div class="my-3 fw-semibold"><span class="badge fs-5 bg-gov-blue me-1"><?= $post_my_query->found_posts ?></span> courses from this partner</div>
+                        <?php while ($post_my_query->have_posts()) : $post_my_query->the_post(); ?>
+                            <?php get_template_part('template-parts/course/single-course') ?>
+                        <?php endwhile; ?>
+                    <?php else : ?>
+                        <p>Oh no! There are no courses that match your filters.</p>
+                        <?php //get_template_part( 'template-parts/content/content-none' ); 
+                        ?>
+                    <?php endif; ?>
 
-	</div>
-    <div class="col-md-4">
-    
-    <?php get_template_part('template-parts/sidebar/taxonomies') ?>
+                </div>
+                <div class="col-md-4">
 
-	</div>
-	</div>
+                    <?php get_template_part('template-parts/sidebar/taxonomies') ?>
+
+                </div>
+            </div>
 
 
 
-<script src="//cdnjs.cloudflare.com/ajax/libs/list.js/2.3.1/list.min.js"></script>
-<script>
+            <script src="//cdnjs.cloudflare.com/ajax/libs/list.js/2.3.1/list.min.js"></script>
+            <script>
+                var courseoptions = {
+                    valueNames: ['coursename', 'coursedesc', 'coursecats', 'coursekeys']
+                };
+                var courses = new List('courselist', courseoptions);
+                document.getElementById('coursecount').innerHTML = courses.update().matchingItems.length;
+                courses.on('searchComplete', function() {
+                    //console.log(upcomingClasses.update().matchingItems.length);
+                    //console.log(courses.update().matchingItems.length);
+                    document.getElementById('coursecount').innerHTML = courses.update().matchingItems.length;
+                });
+            </script>
 
-var courseoptions = {
-    valueNames: [ 'coursename', 'coursedesc', 'coursecats', 'coursekeys' ]
-};
-var courses = new List('courselist', courseoptions);
-document.getElementById('coursecount').innerHTML = courses.update().matchingItems.length;
-courses.on('searchComplete', function(){
-    //console.log(upcomingClasses.update().matchingItems.length);
-    //console.log(courses.update().matchingItems.length);
-    document.getElementById('coursecount').innerHTML = courses.update().matchingItems.length;
-});
-
-</script>
-
-<?php get_footer(); ?>
+            <?php get_footer(); ?>
