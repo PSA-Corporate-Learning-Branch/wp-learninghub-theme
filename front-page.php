f992d79c28b2db8dd80171e108c366021d910962
Allan Haggett <Allan.Haggett@gov.bc.ca>
2024-07-29 18:57:15 +0000

    Remove "journeys" and re-word foundation learning calllout; make whole thing clickable.

--- front-page.php
+++ front-page.php
@@ -19,15 +19,17 @@
                             <div class="icon-square flex-shrink-0 mt-1"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="green-flag"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                                     <path d="M64 32C64 14.3 49.7 0 32 0S0 14.3 0 32V64 368 480c0 17.7 14.3 32 32 32s32-14.3 32-32V352l64.3-16.1c41.1-10.3 84.6-5.5 122.5 13.4c44.2 22.1 95.5 24.8 141.7 7.4l34.7-13c12.5-4.7 20.8-16.6 20.8-30V66.1c0-23-24.2-38-44.8-27.7l-9.6 4.8c-46.3 23.2-100.8 23.2-147.1 0c-35.1-17.6-75.4-22-113.5-12.5L64 48V32z" />
                                 </svg></div>
+                            <a href="/learninghub/foundational-corporate-learning/" class="text-decoration-none">
                             <div class="ms-3">
                                 <h3 class="gov-green">Start here</h3>
-                                <p>Wondering where to start? These <a href="/learninghub/foundational-corporate-learning/">learning journeys</a> will guide you.</p>
+                                <p><span class="text-decoration-underline">Foundational learning</span> for all employees and people leaders in their first year and beyond.</p>
                                 <!-- <p class="mb-1"><strong>Learning journeys</strong></p>
                                 <ul class="mb-2">
                                     <li><a href="/learninghub/foundational-corporate-learning/">All Employees</a></li>
                                     <li><a href="/learninghub/foundational-corporate-learning/">People Leaders</a></li>
                                 </ul> -->
                             </div>
+                            </a>
                         </div>
                     </div>
                 </div>
