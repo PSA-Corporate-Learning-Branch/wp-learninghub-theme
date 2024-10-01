9d7bf72680326c7daf333954b3233844c437cad7
Allan Haggett <Allan.Haggett@gov.bc.ca>
2024-10-01 22:32:22 +0000

    Remove groups from audit.

--- page-taxonomy-audit.php
+++ page-taxonomy-audit.php
@@ -19,7 +19,6 @@ $post_args = array(
 );
 $post_my_query = null;
 $post_my_query = new WP_Query($post_args);
-$nogroups = [];
 $noauds = [];
 $notops = [];
 $nomethods = [];
@@ -27,14 +26,12 @@ if ($post_my_query->have_posts()) :
     while ($post_my_query->have_posts()) : $post_my_query->the_post();
         $name = get_the_title();
         $slug = basename(get_permalink());
-        $grp = get_the_terms($post_my_query->ID, 'groups', '', ', ', ' ');
         $top = get_the_terms($post_my_query->ID, 'topics', '', ', ', ' ');
         $aud = get_the_terms($post_my_query->ID, 'audience', '', ', ', ' ');
         $dm = get_the_terms($post_my_query->ID, 'delivery_method', '', ', ', ' ');
         $ext = get_the_terms($post_my_query->ID, 'external_system', '', ', ', ' ');
         $taxs = [$grp[0]->name, $top[0]->name, $aud[0]->name, $dm[0]->name, $ext[0]->name];
         $c = [$slug, $name, $taxs];
-        if (empty($grp[0]->name)) array_push($nogroups, $c);
         if (empty($top[0]->name)) array_push($notops, $c);
         if (empty($aud[0]->name)) array_push($noauds, $c);
         if (empty($dm[0]->name)) array_push($nomethods, $c);
@@ -58,7 +55,6 @@ endif;
                     <div class="p-3 mb-3 bg-light-subtle rounded-3">
                         <div><a href="/learninghub/course/<?= $nt[0] ?>"><?= $nt[1] ?></a></div>
                         <div>Platform: <?= $nt[2][4] ?></div>
-                        <div>Group: <?= $nt[2][0] ?></div>
                         <div>Topic: <?= $nt[2][1] ?></div>
                         <div>Audience: <?= $nt[2][2] ?></div>
                         <div>Delivery: <?= $nt[2][3] ?></div>
@@ -66,19 +62,6 @@ endif;
                 <?php endforeach ?>
             </details>
 
-            <details>
-                <summary>Group <span class="badge bg-warning text-black"><?= count($nogroups) ?></span></summary>
-                <?php foreach ($nogroups as $ng): ?>
-                    <div class="p-3 mb-3 bg-light-subtle rounded-3">
-                        <div><a href="/learninghub/course/<?= $ng[0] ?>"><?= $ng[1] ?></a></div>
-                        <div>Platform: <?= $ng[2][4] ?></div>
-                        <div>Group: <?= $ng[2][0] ?></div>
-                        <div>Topic: <?= $ng[2][1] ?></div>
-                        <div>Audience: <?= $ng[2][2] ?></div>
-                        <div>Delivery: <?= $ng[2][3] ?></div>
-                    </div>
-                <?php endforeach ?>
-            </details>
 
             <details>
                 <summary>Audience <span class="badge bg-warning text-black"><?= count($noauds) ?></span></summary>
@@ -86,7 +69,6 @@ endif;
                     <div class="p-3 mb-3 bg-light-subtle rounded-3">
                         <div><a href="/learninghub/course/<?= $a[0] ?>"><?= $a[1] ?></a></div>
                         <div>Platform: <?= $a[2][4] ?></div>
-                        <div>Group: <?= $a[2][0] ?></div>
                         <div>Topic: <?= $a[2][1] ?></div>
                         <div>Audience: <?= $a[2][2] ?></div>
                         <div>Delivery: <?= $a[2][3] ?></div>
