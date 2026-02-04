d1d391903e8050aa767512820ca97719c7ea7373
Nori Sinclair <nori.sinclair@gov.bc.ca>
2026-02-04 20:49:56 +0000

    front page mockup with comms changes

--- front-page.php
+++ front-page.php
@@ -1,5 +1,4 @@
-<?php get_header() ?>
-<div id="content">
+<?php get_header() ?> <div id="content">
     <div class="d-flex p-4 p-md-5 align-items-center" style="height: 18vh; min-height: 100px; background: rgb(35,64,117); background: linear-gradient(87deg, rgba(35,64,117,1) 0%, rgba(0,120,100,1) 50%, rgba(227,168,43,1) 91%);">
         <div class="container-lg py-4 py-md-5">
             <h1 class="text-white title">Welcome to the LearningHUB</h1>
@@ -16,8 +15,49 @@
                     <p>Get the details on <a href="/learninghub/foundational-corporate-learning/">Mandatory and Foundational learning</a> for all employees and people leaders in their first year and beyond.</p>
                 </div>
                 <div class="col-lg-4 pe-lg-0">
-                    <div class="mt-4 mt-lg-0 card shadow-sm rounded">
-                        <?php
+                    <div class="card shadow-sm">
+                        <div class="bg-gov-blue card-header pt-3">
+                            <h4 class="text-white mb-1">New and updated courses</h4>
+                        </div>
+                        <div class="card-body fs-6">
+                            <ul class="card-text" style="line-height: 1.75;">
+                                <li><a href="#">2SLGBTQIA+ Learning Series</a></li>
+                                <li><a href="#">Moose Hide Campaign Day (2026) - In-person and Online Gathering and Day of Fasting on May 14, 2026</a></li>
+                                <li><a href="#">L@WW 2023 Session Recordings</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card shadow-sm mt-3">
+                        <div class="card-body fs-6">
+                            <h3 class="card-title fs-4">Corporate learning newsletter</h3>
+                            <p class="card-text">Stay updated about all things corporate learning with our monthly email newsletter.</p>
+                            <a href="#" class="btn btn-primary">Subscribe to the newsletter</a>
+                        </div>
+                    </div>
+                </div>
+            </div> <?php
+            $sticky = get_option('sticky_posts');
+            // check if there are any
+            if (!empty($sticky)) :
+                // optional: sort the newest IDs first
+                rsort($sticky);
+                // override the query
+                $args = array(
+                    'post__in' => $sticky
+                );
+                query_posts($args);
+            ?> <?php while (have_posts()) : ?> <?php the_post() ?> <?php if (has_post_thumbnail($post->ID)) : ?> <?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'large'); ?> <a href="<?= the_permalink() ?>" class="text-decoration-none p-0">
+                <img alt="" aria-label="<?php the_title() ?>" class="mt-4 border border-2 border-gov-blue border-bottom-0 rounded-top w-100 object-fit-contain" src="<?php echo $image[0]; ?>">
+            </a> <?php endif; ?> <div class="bg-gov-blue px-3 py-2 rounded-bottom shadow-sm">
+                <h3 class="text-white mb-0 p-2"><?php the_title() ?></h3>
+            </div> <?php endwhile ?> <?php endif ?>
+        </div>
+    </div>
+</div>
+<div class="bg-secondary-subtle pt-4">
+    <div class="container-lg p-lg-5 p-4 bg-light-subtle">
+        <h3>What's new in corporate learning?</h3> 
+        <?php
                         $sticky_posts = get_option('sticky_posts');
                         $newsargs = array(
                             'posts_per_page' => 1, // Only get one post
@@ -29,69 +69,87 @@
                         $recent_post = new WP_Query($newsargs);
                         if ($recent_post->have_posts()) :
                             while ($recent_post->have_posts()) : $recent_post->the_post();
-                        ?>
-                                <div class="bg-gov-green rounded-top">
-                                    <?php if (has_post_thumbnail($recent_post->ID)) : ?>
-                                        <?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($recent_post->ID), 'large'); ?>
-                                        <a href="<?= the_permalink() ?>" class="text-decoration-none p-0">
-                                            <img alt="" aria-label="<?= the_title() ?>" style="height:12vh;" class="card-img-top object-fit-cover rounded-top opacity-50" src="<?php echo $image[0]; ?>">
-                                        </a>
-                                    <?php endif; ?>
-                                </div>
-                                <div class="card-body fs-6">
-                                    <h3 class=" card-title fs-4">What's new?</h3>
-
-                                    <h4 class="fs-5"><a href="<?= the_permalink() ?>"><?= the_title() ?></a></h4>
-                                    <p class="card-text"><?= the_excerpt() ?></p>
-                            <?php endwhile;
+                        ?> 
+                        <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3 mt-4 gx-5">
+            <div class="d-flex col mb-3">
+                <div class="flex-shrink-0 blue-fill align-self-start pt-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" class="icon-lg">
+                        <!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
+                        <path d="M288 88C288 74.7 298.7 64 312 64C457.8 64 576 182.2 576 328C576 341.3 565.3 352 552 352C538.7 352 528 341.3 528 328C528 208.7 431.3 112 312 112C298.7 112 288 101.3 288 88zM144 160C170.5 160 192 181.5 192 208L192 432C192 458.5 213.5 480 240 480C266.5 480 288 458.5 288 432C288 405.5 266.5 384 240 384C231.2 384 224 376.8 224 368L224 304C224 295.2 231.2 288 240 288C319.5 288 384 352.5 384 432C384 511.5 319.5 576 240 576C160.5 576 96 511.5 96 432L96 208C96 181.5 117.5 160 144 160zM312 160C404.8 160 480 235.2 480 328C480 341.3 469.3 352 456 352C442.7 352 432 341.3 432 328C432 261.7 378.3 208 312 208C298.7 208 288 197.3 288 184C288 170.7 298.7 160 312 160z" />
+                    </svg>
+                </div>
+                <div class="flex-grow-1 ms-3">
+                    <h4 class="mb-1">Blog</h4>
+                    <div class="mt-2 card shadow-sm rounded">
+                        <div class="rounded-top"> <?php if (has_post_thumbnail($recent_post->ID)) : ?> <?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($recent_post->ID), 'large'); ?> <a href="<?= the_permalink() ?>" class="text-decoration-none p-0">
+                                <img alt="" aria-label="<?= the_title() ?>" style="height:10vh;" class="card-img-top object-fit-cover rounded-top " src="<?php echo $image[0]; ?>">
+                            </a> <?php endif; ?> </div>
+                        <div class="card-body fs-6">
+                            <h5 class="fs-5"><a href="<?= the_permalink() ?>"><?= the_title() ?></a></h5>
+                            <p class="card-text"><?= the_excerpt() ?></p> <?php endwhile;
                             wp_reset_postdata(); // Reset query
                         endif;
                             ?>
-                            <p class="card-text"><a href="/learninghub/news">Read the latest news</a></p>
-                                </div>
+                        </div>
                     </div>
+                    <ul class="mt-2">
+                        <li><a href="/learninghub/news">Read the latest blog posts</a></li>
+                    </ul>
                 </div>
             </div>
-            <?php
-            $sticky = get_option('sticky_posts');
-            // check if there are any
-            if (!empty($sticky)) :
-                // optional: sort the newest IDs first
-                rsort($sticky);
-                // override the query
-                $args = array(
-                    'post__in' => $sticky
-                );
-                query_posts($args);
-            ?>
-                <?php while (have_posts()) : ?>
-                    <?php the_post() ?>
-                    <?php if (has_post_thumbnail($post->ID)) : ?>
-                        <?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'large'); ?>
-                        <a href="<?= the_permalink() ?>" class="text-decoration-none p-0">
-                            <img alt="" aria-label="<?php the_title() ?>" class="mt-4 border border-2 border-gov-blue border-bottom-0 rounded-top w-100 object-fit-contain" src="<?php echo $image[0]; ?>">
-                        </a>
-                    <?php endif; ?>
-                    <div class="bg-gov-blue px-3 py-2 rounded-bottom shadow-sm">
-                        <h3 class="text-white mb-0 p-2"><?php the_title() ?></h3>
+            <div class="d-flex col mb-3">
+                <div class="flex-shrink-0 blue-fill align-self-start pt-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" class="icon-lg">
+                        <!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
+                        <path d="M128 160C128 124.7 156.7 96 192 96L544 96C579.3 96 608 124.7 608 160L608 400L512 400L512 384C512 366.3 497.7 352 480 352L416 352C398.3 352 384 366.3 384 384L384 400L254.9 400C265.8 381.2 272 359.3 272 336C272 265.3 214.7 208 144 208C138.6 208 133.2 208.3 128 209L128 160zM333 512C327.9 487.8 316.7 465.9 300.9 448L608 448C608 483.3 579.3 512 544 512L333 512zM64 336C64 291.8 99.8 256 144 256C188.2 256 224 291.8 224 336C224 380.2 188.2 416 144 416C99.8 416 64 380.2 64 336zM0 544C0 491 43 448 96 448L192 448C245 448 288 491 288 544C288 561.7 273.7 576 256 576L32 576C14.3 576 0 561.7 0 544z" />
+                    </svg>
+                </div>
+                <div class="flex-grow-1 ms-3">
+                    <h4 class="mb-1">Case study</h4>
+                    <div class="mt-2 card shadow-sm rounded">
+                        <div class="rounded-top"> <a href="http://localhost:8181/learninghub/working-and-connecting-through-change/" class="text-decoration-none p-0">
+                                <img alt="" aria-label="Working and Connecting Through Change" style="height:10vh;" class="card-img-top object-fit-cover rounded-top " src="http://localhost:8181/learninghub/wp-content/uploads/2025/11/Reintegration-1.png">
+                            </a> </div>
+                        <div class="card-body fs-6">
+                            <h5 class="fs-5"><a href="http://localhost:8181/learninghub/working-and-connecting-through-change/">Working and Connecting Through Change</a></h5>
+                            <p class="card-text"></p>
+                            <p>Change is our only constant, and learning to navigate change isn’t always simple. Working together and leaning on each other for support can clear and […]</p>
+                            <p></p>
+                        </div>
                     </div>
-                <?php endwhile ?>
-            <?php endif ?>
+                    <ul class="mt-2">
+                        <li><a href="/learninghub/news">Read the latest case studies</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="d-flex col mb-3">
+                <div class="flex-shrink-0 blue-fill align-self-start pt-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" class="icon-lg">
+                        <!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
+                        <path d="M525.2 82.9C536.7 88 544 99.4 544 112L544 528C544 540.6 536.7 552 525.2 557.1C513.7 562.2 500.4 560.3 490.9 552L444.3 511.3C400.7 473.2 345.6 451 287.9 448.3L287.9 544C287.9 561.7 273.6 576 255.9 576L223.9 576C206.2 576 191.9 561.7 191.9 544L191.9 448C121.3 448 64 390.7 64 320C64 249.3 121.3 192 192 192L276.5 192C338.3 191.8 397.9 169.3 444.4 128.7L491 88C500.4 79.7 513.9 77.8 525.3 82.9zM288 384L288 384.2C358.3 386.9 425.8 412.7 480 457.6L480 182.3C425.8 227.2 358.3 253 288 255.7L288 384z" />
+                    </svg>
+                </div>
+                <div class="flex-grow-1 ms-3">
+                    <h4 class="mb-1">Announcements</h4>
+                    <!-- Add code to bring in announcement titles from posts tagged as announcement -->
+                    <ul>
+                        <li class="mb-2"><a href="#">L@WW Your Time to Learn Q&A results posted</a> <span class="text-muted fs-6">Feb 20, 2026</span></li>
+                        <li class="mb-2"><a href="#">New SBCPS cohorts open for registration</a> <span class="text-muted fs-6">Feb 5, 2026</span> </li>
+                        <li class="mb-2"><a href="#">Scheduled Moodle outage Feb 14, 2026</a> <span class="text-muted fs-6">Jan 25, 2026</span></li>
+                        <li><a href="#">Read all announcements</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
-    </div>
-
-</div>
-<div class="bg-secondary-subtle pt-4">
-    <div class="container-lg p-lg-5 p-4 bg-light-subtle">
+        <hr>
         <h3>Where the learning happens</h3>
         <p>The LearningHUB includes courses from more than a dozen learning platforms. <a href="/learninghub/learning-systems/">Check out the full list of learning platforms</a>.</p>
         <h4>Featured platforms</h4>
-<div class="row">
+        <div class="row">
             <div class="col-md-6">
                 <div class="card shadow-sm">
                     <div class="bg-gov-green card-header pt-3">
                         <h4 class="text-white mb-1">PSA Learning System</h4>
-                        
                     </div>
                     <div class="card-body">
                         <p class="card-text">The PSA Learning System has the largest selection of courses available for registration for BCPS employees. It acts as a primary training record for current and completed learning.</p>
@@ -101,13 +159,11 @@
                         </ul>
                     </div>
                 </div>
-
             </div>
             <div class="col-md-6">
                 <div class="card shadow-sm mt-3 mt-md-0">
                     <div class="bg-gov-green card-header pt-3">
                         <h4 class="text-white mb-1">Your Digital Workplace</h4>
-                        
                     </div>
                     <div class="card-body">
                         <p class="card-text">Your Digital Workplace is the hub for discovering the Microsoft 365 (M365) tools available to BC Public Service employees to support their daily work.</p>
@@ -122,5 +178,4 @@
             </div>
         </div>
     </div>
-</div>
-<?php get_footer() ?>
+</div> <?php get_footer() ?>
